b2b932be0caf0d65cfba008c4911bef887dc55f3
ROSEQUEEN NWEZEH <rosequeen.nwezeh@gmail.com>
2025-07-10 04:31:29 +0100

    my third code

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+let number = -5
+
+if(number > 0){
+    console.log(`${number} is positive to the console`);
+    else if(number < 0)
+        console.log(`${number} is negative to the console`);
+      else { console.log(`${number} is zero`);
+}
+
+}
