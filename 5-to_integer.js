2906c12d78f3d1c2281f89d2d436b0a777833d44
ROSEQUEEN NWEZEH <rosequeen.nwezeh@gmail.com>
2025-07-11 10:42:59 +0100

    fifth code

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const argv = Process.argv.slice(2);
 
 const num = 89(argv);
 
-if (!isNaN(num) && arg !== undefined && arg.trim() !== '') {
+if (!isNaN(num) && argv !== undefined && argv.trim() !== '') {
   console.log(`My number: ${parseInt(num)}`);
 } else {
   console.log('Not a number');
